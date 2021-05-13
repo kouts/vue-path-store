c4d41f24263bf03ca4510bbf240166224379a311
Kouts <koutsaftakis@gmail.com>
2021-05-13 12:10:18 +0300

    BREAKING CHANGE: renamed delete method

--- src/pathStore.js
+++ src/pathStore.js
@@ -6,24 +6,23 @@ import { ARRAY_METHODS } from './constants.js'
 const createPathStore = (state) => {
   const store = Vue.observable(state)
 
-  store.set = (path, value) => {
-    setMany(store, path, value)
+  const methods = {
+    set(path, value) {
+      setMany(store, path, value)
+    },
+    toggle(path) {
+      setOne(store, path, !getByPath(store, path))
+    },
+    get(path) {
+      return path ? getByPath(store, path) : store
+    },
+    del(path) {
+      deleteMany(store, path)
+    }
   }
 
-  store.toggle = (path) => {
-    setOne(store, path, !getByPath(store, path))
-  }
-
-  store.get = (path) => {
-    return path ? getByPath(store, path) : store
-  }
-
-  store.delete = (path) => {
-    deleteMany(store, path)
-  }
-
-  ARRAY_METHODS.forEach((method) => {
-    store[method] = (...args) => {
+  const arrayMethods = ARRAY_METHODS.reduce((acc, method) => {
+    const fn = (...args) => {
       const path = args.shift()
       const arr = getByPath(store, path)
       if (!isArray(arr)) {
@@ -31,9 +30,10 @@ const createPathStore = (state) => {
       }
       arr[method](...args)
     }
-  })
+    return Object.assign(acc, { [method]: fn })
+  }, {})
 
-  return store
+  return Object.assign(store, methods, arrayMethods)
 }
 
 export { createPathStore }
