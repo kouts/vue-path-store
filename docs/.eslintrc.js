8d0f930714406ede932314533c95fe5df6f0d64e
Kouts <koutsaftakis@gmail.com>
2021-06-21 17:23:29 +0300

    chore: added specific folder rules for eslint and prettier

---
+++ docs/.eslintrc.js
@@ -0,0 +1,14 @@
+/* In app/containers/.eslintrc.js */
+module.exports = {
+  rules: {
+    // Print width setting
+    "max-len": [
+      "error",
+      {
+        code: 80,
+        ignoreComments: true,
+        ignoreUrls: true,
+      },
+    ],
+  },
+};
